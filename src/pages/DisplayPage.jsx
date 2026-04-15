3b72849cf3b7b76a15b2ac2255a232ec84a61352
ManhDuc <phungmanhduc2209@gmail.com>
2026-04-16 01:59:21 +0700

    update scan page + add speech

--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -32,17 +32,28 @@ function DisplayPage() {
   });
 
   function speak(text) {
-  if (!window.speechSynthesis) return;
+    if (!window.speechSynthesis) return;
 
-  const utterance = new SpeechSynthesisUtterance(text);
+    const utterance = new SpeechSynthesisUtterance(text);
 
-  utterance.lang = "vi-VN"; // tiếng Việt
-  utterance.rate = 1;       // tốc độ
-  utterance.pitch = 1;      // cao độ
+    const voices = window.speechSynthesis.getVoices();
 
-  window.speechSynthesis.cancel(); // tránh đọc chồng
-  window.speechSynthesis.speak(utterance);
-}
+    // 🔥 chọn giọng tiếng Việt
+    const vietnameseVoice =
+      voices.find((v) => v.lang === "vi-VN") ||
+      voices.find((v) => v.lang.includes("vi"));
+
+    if (vietnameseVoice) {
+      utterance.voice = vietnameseVoice;
+    }
+
+    utterance.lang = "vi-VN";
+    utterance.rate = 0.95;
+    utterance.pitch = 1;
+
+    window.speechSynthesis.cancel();
+    window.speechSynthesis.speak(utterance);
+  }
 
   const [highlightId, setHighlightId] = useState(null);
   const highlightTimerRef = useRef(null);
